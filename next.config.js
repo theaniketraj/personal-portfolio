63d1940329e60c8e0e98476e620a4e504387bca6
Aniket Raj <118242466+theaniketraj@users.noreply.github.com>
2025-08-05 22:04:20 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,6 +8,18 @@ const nextConfig = {
 };
 
 module.exports = {
+    future: {
+    webpack5: true,
+  },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Increase Node.js memory limit to 4GB
+    config.node = {
+      fs: 'empty',
+      ...config.node,
+    };
+
+    return config;
+  },
   experimental: {
     largePageDataBytes: 1024 * 1024, // for example, 1 MB
   }
