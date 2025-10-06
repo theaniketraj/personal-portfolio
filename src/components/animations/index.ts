898e420f02ad8e70a8bea2d8e3fb38cca026b7c1
theaniketraj <cs2258.diatm@gmail.com>
2025-10-06 19:17:23 +0530

    feat: add new file for hardware-accelerated animation components and hooks

---
+++ src/components/animations/index.ts
@@ -0,0 +1,22 @@
+// Hardware-Accelerated Animation Components
+export {
+    AnimatedCounter,
+    HardwareAnimated,
+    HoverEffect,
+    LoadingSkeleton,
+    OptimizedImage,
+    ParallaxSection,
+    StaggeredContainer
+} from './HardwareAccelerated';
+
+// Animation Hooks
+export {
+    useAnimationPerformance,
+    useHardwareAcceleration,
+    useHoverAnimation,
+    useOptimizedImageLoading,
+    useOptimizedParallax,
+    useReducedMotion,
+    useScrollAnimation,
+    useStaggeredAnimation
+} from '../../hooks/useHardwareAnimations';
