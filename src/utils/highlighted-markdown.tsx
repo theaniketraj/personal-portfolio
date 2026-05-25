3739f82c64dc396d98e05d9f06a07315569724b0
theaniketraj <aniketar999@gmail.com>
2026-05-26 00:32:03 +0530

    chore: update Node.js version to 20 in netlify.toml and switch to esm imports in highlighted-markdown.tsx

--- src/utils/highlighted-markdown.tsx
+++ src/utils/highlighted-markdown.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import bash from 'react-syntax-highlighter/dist/cjs/languages/prism/bash';
-import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css';
-import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
-import json from 'react-syntax-highlighter/dist/cjs/languages/prism/json';
-import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx';
-import markdown from 'react-syntax-highlighter/dist/cjs/languages/prism/markdown';
-import tsx from 'react-syntax-highlighter/dist/cjs/languages/prism/tsx';
-import ts from 'react-syntax-highlighter/dist/cjs/languages/prism/typescript';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import bash from 'react-syntax-highlighter/dist/esm/languages/prism/bash';
+import css from 'react-syntax-highlighter/dist/esm/languages/prism/css';
+import js from 'react-syntax-highlighter/dist/esm/languages/prism/javascript';
+import json from 'react-syntax-highlighter/dist/esm/languages/prism/json';
+import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
+import markdown from 'react-syntax-highlighter/dist/esm/languages/prism/markdown';
+import tsx from 'react-syntax-highlighter/dist/esm/languages/prism/tsx';
+import ts from 'react-syntax-highlighter/dist/esm/languages/prism/typescript';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 SyntaxHighlighter.registerLanguage('javascript', js);
 SyntaxHighlighter.registerLanguage('css', css);
